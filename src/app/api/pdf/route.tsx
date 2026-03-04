24dfc512ccfc7753491a0446f185f512c5426dbe
Agbessi <hagbessi@gmail.com>
2026-03-04 09:24:28 +0100

    fix: Buffer.from Uint8Array NextResponse

--- src/app/api/pdf/route.tsx
+++ src/app/api/pdf/route.tsx
@@ -43,7 +43,7 @@ export async function POST(request: NextRequest) {
 
     const finalBytes = await pdfDoc.save();
 
-    return new NextResponse(finalBytes, {
+    return new NextResponse(Buffer.from(finalBytes), {
       status: 200,
       headers: {
         "Content-Type": "application/pdf",
